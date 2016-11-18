63e30ddee9c657b781e82ccb34e1ec609c53a5f2
Nagavenkat Adurthi <n.adurthi@gmail.com>
2016-11-18 17:18:36 -0600

    map

--- cpp_examples/oop/hacker1.cpp
+++ cpp_examples/oop/hacker1.cpp
@@ -14,12 +14,26 @@ int main() {
 
 
     std::map<int,int> F;
-    F[1]=2;
-    F[2]=3;
+    F[2]=20;
+    F[1]=30;
+    F[4]=200;
+    F[4]=300;
+    F.insert ( std::pair<int,int>(5,212) );
+    F.insert ( std::pair<int,int>(4,213) );
+
     std::vector<int> V;
     V.push_back(1);
     V.push_back(2);
     V.push_back(3);
+
+    //iterator over map
+    for(auto const & pair : F){
+        cout<<"map 1 F key = "<<pair.first<<" value = "<<pair.second<<endl;
+    }
+
+    for(std::map<int,int>::iterator it=F.begin();it!=F.end();it++){
+        cout<<"map 2 F key = "<<it->first<<" value = "<<it->second<<endl;
+    }
 
     // Iterator type 1 for vector
     for(auto value: V ){
@@ -27,9 +41,11 @@ int main() {
     }
 
 
-    // for(auto const& value: V ){
-    // 	cout<<"vector iterator = "<<value <<endl;
-    // }
+    for(std::vector<int>::iterator it=V.begin(); it!=V.end(); it++ ){
+    	cout<<"vector 2 iterator = "<<*it <<endl;
+    }
+
+
 
     return 0;
 }
